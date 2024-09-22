6b92dd7eac725238292eb8b8059b519d1eb0bb95
Álan Crístoffer <acristoffers@startmail.com>
2024-09-22 13:53:31 +0200

    chore: update dependencies

--- flake.nix
+++ flake.nix
@@ -1,28 +1,30 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-
     flake-utils.url = "github:numtide/flake-utils";
-
-    naersk.url = "github:nix-community/naersk";
-    naersk.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = { self, flake-utils, naersk, nixpkgs }:
+  outputs = { self, flake-utils, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         version = "1.0.0";
         pkgs = (import nixpkgs) { inherit system; };
-        naersk' = pkgs.callPackage naersk { };
+        nativeBuildInputs = with pkgs; [ cmake pkg-config rustc cargo ];
         buildInputs = [ ];
-        mkPackage = { name, buildInputs ? [ ] }: naersk'.buildPackage {
-          cargoBuildOptions = opts: opts ++ [ "--package" name ];
-          inherit buildInputs;
-          inherit name;
+        mkPackage = { name, buildInputs ? [ ] }: pkgs.rustPlatform.buildRustPackage {
+          cargoBuildOptions = "--package ${name}";
+          pname = name;
           inherit version;
-          nativeBuildInputs = with pkgs; [ cmake pkg-config ];
+          inherit buildInputs;
+          inherit nativeBuildInputs;
+          cargoLock = {
+            lockFile = ./Cargo.lock;
+            outputHashes = {
+              "tree-sitter-wbproto-0.0.1" = "sha256-oBo41rvptzQzsyD6chjbvOfiH9+SVVX+s3+yDvSXWk4=";
+            };
+          };
           src = ./.;
           postInstall = "
-            cp -r target/release/share $out/share
+            cp -r target/*/release/share $out/share
           ";
         };
       in
